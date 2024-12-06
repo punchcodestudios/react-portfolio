0063bd85e9a9ef5e2dc295f5aeff17332c619154
Patrick Schandler <pschandler@gmail.com>
2024-12-06 16:22:32 -0600

    next problem is environment variables

--- web/src/api/apiClient.ts
+++ web/src/api/apiClient.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
+import {} from "dotenv/config";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api/",
+  baseURL: process.env.MONGO_DB_CONNECTION,
   withCredentials: true,
 });
 
