e117f7bb6bb360ad132a182f89b088eab972e3ea
Patrick Schandler <pschandler@gmail.com>
2024-12-06 19:29:23 -0600

    update helmet csp

--- startup/prod.js
+++ startup/prod.js
@@ -1,7 +1,15 @@
 const helmet = require("helmet");
+const contentSecurityPolicy = require("helmet-csp");
 const compression = require("compression");
 
 module.exports = function (app) {
   app.use(helmet());
+  app.use(
+    contentSecurityPolicy({
+      directives: {
+        connectSrc: ["self", "https://api.rawg.io/api/*"],
+      },
+    })
+  );
   app.use(compression());
 };
