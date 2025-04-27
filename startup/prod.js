5af2b2ab2e190fcf417ad78a3276f2959244b98b
Patrick Schandler <pschandler@gmail.com>
2025-04-26 19:30:31 -0500

    https: unsafe-inline

--- startup/prod.js
+++ startup/prod.js
@@ -10,15 +10,7 @@ module.exports = function (app) {
         defaultSrc: ["'self'"],
         connectSrc: ["'self'", "api.rawg.io"],
         imgSrc: ["'self'", "media.rawg.io"],
-        scriptSrc: [
-          "'self'",
-          "http://localhost:3000",
-          "localhost:3000",
-          "localhost",
-          "https://www.punchcodestudios.com",
-          "www.punchcodestudios.com",
-          "punchcodestudios.com",
-        ],
+        scriptSrc: ["https: unsafe-inline"],
       },
     })
   );
