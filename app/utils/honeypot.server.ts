f4dc977141c0789a8e1fb6a56249adcdb9cdb6bb
Patrick Schandler <pschandler@gmail.com>
2025-11-17 08:50:37 -0600

    adding back reference to honeypot env variable

--- app/utils/honeypot.server.ts
+++ app/utils/honeypot.server.ts
@@ -4,7 +4,7 @@ export const honeypot = new Honeypot({
   validFromFieldName: process.env.TESTING ? null : undefined,
   // 🐨 add an encryptionSeed option to the honeypot constructor
   // and set it to the HONEYPOT_SECRET enviroenment variable
-  // encryptionSeed: process.env.HONEYPOT_SECRET,
+  encryptionSeed: process.env.HONEYPOT_SECRET,
 });
 
 process.on("unhandledRejection", (reason, p) => {
